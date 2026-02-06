61e182779d2e2da145c2999aa5942729ac72d86d
Vidit Khandelwal <59656953+vkhandelwal48@users.noreply.github.com>
2026-02-06 22:19:21 +0530

    back to normal

--- ReactPrep/RefsAndPortals/src/components/Player.jsx
+++ ReactPrep/RefsAndPortals/src/components/Player.jsx
@@ -2,16 +2,16 @@ import { useRef, useState } from "react";
 
 export default function Player() {
   const playerName = useRef();
-  // const [enteredPlayerName, setEnteredPlayerName] = useState("");
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
 
   function handleClick() {
-    // setEnteredPlayerName(playerName.current.value);
+    setEnteredPlayerName(playerName.current.value);
     playerName.current.value = ""; // changing dom value directly, not recommended but works for this example
   }
 
   return (
     <section id="player">
-      <h2>Welcome {playerName.current ? playerName.current.value : "unknown entity"}</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
         <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
