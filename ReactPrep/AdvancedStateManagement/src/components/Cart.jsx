93b28260fa0052f74ef7b1e460407f381446f366
Vidit Khandelwal <59656953+vkhandelwal48@users.noreply.github.com>
2026-02-08 00:24:05 +0530

    migrating entire project to context api

--- ReactPrep/AdvancedStateManagement/src/components/Cart.jsx
+++ ReactPrep/AdvancedStateManagement/src/components/Cart.jsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
 import { CartContext } from "../store/shopping-cart-context";
 
-export default function Cart({ onUpdateItemQuantity }) {
-  const { items } = useContext(CartContext); // or use(CartContext) if using React 19+
+export default function Cart() {
+  const { items, updateItemQuantity } = useContext(CartContext); // or use(CartContext) if using React 19+
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
@@ -24,11 +24,11 @@ export default function Cart({ onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
