af78c3d58a97446ad6ff5065228051dfc62652f2
Vidit Khandelwal <59656953+vkhandelwal48@users.noreply.github.com>
2026-02-07 16:10:43 +0530

    Updated Result Modal

--- ReactPrep/RefsAndPortals/src/components/ResultModal.jsx
+++ ReactPrep/RefsAndPortals/src/components/ResultModal.jsx
@@ -1,7 +1,8 @@
 import { useImperativeHandle, useRef } from "react";
 
-export default function ResultModal({ ref, result, targetTime }) {
+export default function ResultModal({ ref, remainingTime, targetTime, onReset }) {
 	const dialogRef = useRef();
+	const score = Math.round((1 -  remainingTime / (targetTime * 1000)) * 100)
 	useImperativeHandle(ref, () => {
 		return {
 			open() {
@@ -11,10 +12,10 @@ export default function ResultModal({ ref, result, targetTime }) {
 	});
 	return (
 		<dialog ref={dialogRef} className="result-modal">
-			<h2>You {result}</h2>
+			<h2>{remainingTime <= 0 ? "You lost" : `Your Score: ${score}`}!</h2>
 			<p>The target time was <strong>{targetTime} seconds.</strong></p>
-			<p>You stopped the timer with <strong>X seconds left.</strong></p>
-			<form method="dialog">
+			<p>You stopped the timer with <strong>{(remainingTime / 1000).toFixed(2)} seconds left.</strong></p>
+			<form method="dialog" onSubmit={onReset}>
 				<button>Close</button>
 			</form>
 		</dialog>
