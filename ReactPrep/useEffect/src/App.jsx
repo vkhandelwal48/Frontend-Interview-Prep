7cd8ce587d8bcd78427049f4d707f0c386b204bf
Vidit Khandelwal <59656953+vkhandelwal48@users.noreply.github.com>
2026-02-11 15:31:38 +0530

    problems with dependency array values

--- ReactPrep/useEffect/src/App.jsx
+++ ReactPrep/useEffect/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -64,7 +64,7 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(() => {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -75,7 +75,7 @@ function App() {
       'selectedPlaces',
       JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
     );
-  }
+  }, []);
 
   return (
     <>
