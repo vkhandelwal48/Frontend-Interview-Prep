ea05c3dd82de98de4048b592e7bdef287006a5a1
Vidit Khandelwal <59656953+vkhandelwal48@users.noreply.github.com>
2026-06-06 13:33:38 +0530

    useId setup

---
+++ ReactPrep/use-id/src/App.js
@@ -0,0 +1,15 @@
+import Email from './Email';
+
+function App() {
+  return (
+    <>
+      <Email />
+      <article style={{ marginBlock: "1rem" }}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software including versions of Lorem Ipsum.
+      </article>
+      <Email />
+    </>
+  );
+}
+
+export default App;
---
+++ ReactPrep/use-id/src/Email.jsx
@@ -0,0 +1,8 @@
+export default function Email() {
+  return (
+    <>
+      <label htmlFor="email">Email</label>
+      <input id="email" type="email" />
+    </>
+  )
+}
