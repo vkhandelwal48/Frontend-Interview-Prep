af0691c7e0a89e29de86f2a1d9e8b13171544b08
Vidit Khandelwal <59656953+vkhandelwal48@users.noreply.github.com>
2026-06-06 13:42:02 +0530

    first way to use useId

--- ReactPrep/use-id/src/Email.jsx
+++ ReactPrep/use-id/src/Email.jsx
@@ -1,8 +1,16 @@
+import { useId, useRef } from 'react';
+
 export default function Email() {
+  const id1 = useId();
+  const id2 = useId();
+  const ref = useRef(null);
   return (
     <>
-      <label htmlFor="email">Email</label>
-      <input id="email" type="email" />
+      <label htmlFor={id1}>Email</label>
+      <input id={id1} type="email" ref={ref} />
+      <br />
+      <label htmlFor={id2}>Name</label>
+      <input id={id2} type="text" />
     </>
   )
 }
