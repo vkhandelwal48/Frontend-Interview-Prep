918eccad3125be9dfb54741323a84ff422cf047b
Vidit Khandelwal <59656953+vkhandelwal48@users.noreply.github.com>
2026-06-28 22:43:12 +0530

    Layouts & Nested Routes

--- ReactPrep/ReactRouting/src/App.js
+++ ReactPrep/ReactRouting/src/App.js
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -17,8 +18,14 @@ import ProductsPage from './pages/Products';
 // const router = createBrowserRouter(routeDefinitions);
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/products', element: <ProductsPage /> },
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> },
+    ]
+  },
 ]);
 
 function App() {
