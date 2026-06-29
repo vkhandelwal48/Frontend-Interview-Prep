08f6e76eaa7c2771b74624f7f45c8dc6723bb97f
Vidit Khandelwal <59656953+vkhandelwal48@users.noreply.github.com>
2026-06-29 15:37:01 +0530

    Working with useNavigate

--- ReactPrep/ReactRouting/src/pages/Home.js
+++ ReactPrep/ReactRouting/src/pages/Home.js
@@ -1,10 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate('/products');
+  }
+
   return (
     <>
       <h1>My Home Page</h1>
-      <p>Go to <Link to="/products">Products Page</Link>.</p>
+      <p>
+        Go to <Link to="/products">Products Page</Link>.
+      </p>
+      <button onClick={navigateHandler}>Go to Products</button>
     </>
   );
 }
