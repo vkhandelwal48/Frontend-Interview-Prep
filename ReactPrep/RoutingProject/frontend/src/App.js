4b28ac0b7966aa75827b6b15ce15c2543e92555a
Vidit Khandelwal <59656953+vkhandelwal48@users.noreply.github.com>
2026-07-04 22:24:56 +0530

    Working with action() Functions

--- ReactPrep/RoutingProject/frontend/src/App.js
+++ ReactPrep/RoutingProject/frontend/src/App.js
@@ -3,7 +3,7 @@ import Error from "./pages/Error";
 import Home from "./pages/Home";
 import Events, {loader as eventsLoader} from "./pages/Events";
 import EventDetail, {loader as eventDetailLoader } from "./pages/EventDetail";
-import NewEvent from "./pages/NewEvent";
+import NewEvent, { action as newEventAction } from "./pages/NewEvent";
 import EditEvent from "./pages/EditEvent";
 import RootLayout from "./pages/RootLayout";
 import EventsRootLayout from "./pages/EventsRoot";
@@ -58,7 +58,7 @@ const router = createBrowserRouter([
               { path: 'edit', element: <EditEvent /> },
             ],
           },
-          { path: 'new', element: <NewEvent /> },// It would prefer this over the dynamic route, so we need to make sure that this route is defined before the dynamic route.
+          { path: 'new', element: <NewEvent />, action: newEventAction },// It would prefer this over the dynamic route, so we need to make sure that this route is defined before the dynamic route.
         ]
       },
     ]
