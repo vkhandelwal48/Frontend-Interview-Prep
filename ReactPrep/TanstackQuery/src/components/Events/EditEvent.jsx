4e53cb2343ae9330d39e664c13f7bdd9212290ff
Vidit Khandelwal <59656953+vkhandelwal48@users.noreply.github.com>
2026-07-06 02:06:18 +0530

    React Query & React Router

--- ReactPrep/TanstackQuery/src/components/Events/EditEvent.jsx
+++ ReactPrep/TanstackQuery/src/components/Events/EditEvent.jsx
@@ -1,43 +1,46 @@
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams, redirect, useSubmit, useNavigation } from 'react-router-dom';
 import { useQuery, useMutation } from '@tanstack/react-query';
 import { fetchEvent, updateEvent, queryClient } from '../../util/http.js';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
-import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const { state } = useNavigation();
+  const submit = useSubmit();
   const { id } = useParams();
 
-  const { data, isPending, isError, error } = useQuery({
+  const { data, isError, error } = useQuery({
     queryKey: ['events', id],
-    queryFn: ({ signal }) => fetchEvent({ signal, id })
+    queryFn: ({ signal }) => fetchEvent({ signal, id }),
+    staleTime: 10000, // cached data is used without re-fetching behind the scenes if that data is less than 10s old
   });
 
-  const { mutate } = useMutation({
-    mutationFn: updateEvent,
-    onMutate: async (data) => {
-      const newEvent = data.event;
-      await queryClient.cancelQueries({ queryKey: ['events', id] }); 
-      const previousEvent = queryClient.getQueryData(['events', id]);
-      queryClient.setQueryData(['events', id], newEvent);
-      return { previousEvent }; // Return the previous event data so we can roll back if the mutation fails
-    },
-    onError: (error, data, context) => {
-      queryClient.setQueryData(['events', id], context.previousEvent); // Roll back to the previous event data if the mutation fails
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries({
-        queryKey: ['events', id],
-      });// Refetch the event data after the mutation is complete
-    }
-  })
+  // const { mutate } = useMutation({
+  //   mutationFn: updateEvent,
+  //   onMutate: async (data) => {
+  //     const newEvent = data.event;
+  //     await queryClient.cancelQueries({ queryKey: ['events', id] }); 
+  //     const previousEvent = queryClient.getQueryData(['events', id]);
+  //     queryClient.setQueryData(['events', id], newEvent);
+  //     return { previousEvent }; // Return the previous event data so we can roll back if the mutation fails
+  //   },
+  //   onError: (error, data, context) => {
+  //     queryClient.setQueryData(['events', id], context.previousEvent); // Roll back to the previous event data if the mutation fails
+  //   },
+  //   onSettled: () => {
+  //     queryClient.invalidateQueries({
+  //       queryKey: ['events', id],
+  //     });// Refetch the event data after the mutation is complete
+  //   }
+  // })
 
   function handleSubmit(formData) {
-    mutate({ id, event : formData });
-    navigate('../'); // up one level after successful submission
+    // mutate({ id, event : formData });
+    // navigate('../'); // up one level after successful submission
+    submit(formData, { method: 'PUT'}); // any method except GET
   }
 
   function handleClose() {
@@ -46,11 +49,11 @@ export default function EditEvent() {
 
   let content;
 
-  if (isPending) {
-    content = <div className="center">
-      <LoadingIndicator />
-    </div>
-  }
+  // if (isPending) {
+  //   content = <div className="center">
+  //     <LoadingIndicator />
+  //   </div>
+  // }
 
   if (isError) {
     content = <>
@@ -72,12 +75,16 @@ export default function EditEvent() {
   if (data) {
     content = (
       <EventForm inputData={data} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
+        {state === 'submitting' ? <p>Sending data...</p> : (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Update
+            </button>
+          </>
+        )}
       </EventForm>
     )
   }
@@ -88,3 +95,22 @@ export default function EditEvent() {
     </Modal>
   );
 }
+
+export function loader({ params }) {
+  const { id } = params;
+  return queryClient.fetchQuery({
+    queryKey: ['events', id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id })
+  });
+}
+
+export async function action({ request, params }) {
+  const formData = await request.formData();
+  const updatedEventData = Object.fromEntries(formData);
+  await updateEvent({ id: params.id, event: updatedEventData });
+  await queryClient.invalidateQueries(['events'])
+  return redirect('../');
+}
+
+// even though we are using loader from React Router DOM. We still use useQuery Hook
+// as provide or help us with caching. So we don't remove that code.
