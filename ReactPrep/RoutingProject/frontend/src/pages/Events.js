d21f0d7b6904662dcd29e16a9de6af423e81616f
Vidit Khandelwal <59656953+vkhandelwal48@users.noreply.github.com>
2026-07-05 13:44:30 +0530

    Learning defer

--- ReactPrep/RoutingProject/frontend/src/pages/Events.js
+++ ReactPrep/RoutingProject/frontend/src/pages/Events.js
@@ -1,21 +1,33 @@
 // import { json } from 'react-router-dom';
-import { useLoaderData } from 'react-router-dom';
+import { Suspense } from 'react';
+import { useLoaderData, defer, Await } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const data = useLoaderData();
+  // const data = useLoaderData();
 
-  if (data.isError) {
-    return <p>{data.message}</p>;
-  }
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
 
-  const events = data.events;
-  return <EventsList events={events} />;
+  // const events = data.events;
+  // return <EventsList events={events} />;
+
+  const { events } = useLoaderData();
+
+  return (
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+      <Await resolve={events}>
+        {/* once promise is resolved, we can use the data */}
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
@@ -28,6 +40,14 @@ export async function loader() {
   } else {
     // const resData = await response.json();
     // const res = new Response('any data' , { status: 200 });
-    return response;
+    // return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
 }
