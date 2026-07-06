b23b7d24911b24ad6be16cc630c311899a6b99a9
Vidit Khandelwal <59656953+vkhandelwal48@users.noreply.github.com>
2026-07-07 01:49:55 +0530

    Handling Loading States

--- ReactPrep/CreatingCustomHooks/src/components/AvailablePlaces.jsx
+++ ReactPrep/CreatingCustomHooks/src/components/AvailablePlaces.jsx
@@ -2,14 +2,17 @@ import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   // useEffect(async () => {}) // we can't use async directly in useEffect, we can create an async function inside useEffect and call it.
   useEffect(() => {
+    setIsFetching(true);
     async function fetchPlaces() {
       const response = await fetch('http://localhost:3000/places');
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
     fetchPlaces();
     // this code will be executed when the response is received from the server
@@ -20,6 +23,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
