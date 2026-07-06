ab2e40e224ae6e3016f422771e8cd429f8eabf5c
Vidit Khandelwal <59656953+vkhandelwal48@users.noreply.github.com>
2026-07-06 23:58:33 +0530

    How NOT To Send HTTP Requests

--- ReactPrep/CreatingCustomHooks/src/components/AvailablePlaces.jsx
+++ ReactPrep/CreatingCustomHooks/src/components/AvailablePlaces.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  fetch('http://localhost:3000/places').then((response) => {
+    response.json()
+  }).then((resData) => {
+    setAvailablePlaces(resData.places);
+  });// this code will be executed when the response is received from the server
+  // This code will create a infinite loop because the state is being updated on every render.
+  // To fix this, we should use useEffect to fetch the data only once when the component mounts.
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
