b0c95a132223fbd01b147c123d82f16db720bf05
Vidit Khandelwal <59656953+vkhandelwal48@users.noreply.github.com>
2026-07-07 01:35:35 +0530

    Using async / await

--- ReactPrep/CreatingCustomHooks/src/components/AvailablePlaces.jsx
+++ ReactPrep/CreatingCustomHooks/src/components/AvailablePlaces.jsx
@@ -4,13 +4,16 @@ import Places from './Places.jsx';
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
+  // useEffect(async () => {}) // we can't use async directly in useEffect, we can create an async function inside useEffect and call it.
   useEffect(() => {
-    fetch('http://localhost:3000/places').then((response) => {
-      return response.json()
-    }).then((resData) => {
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
       setAvailablePlaces(resData.places);
-    });// this code will be executed when the response is received from the server
-    // This code will create a infinite loop because the state is being updated on every render.
+    }
+    fetchPlaces();
+    // this code will be executed when the response is received from the server
+    // This code will create an infinite loop because the state is being updated on every render.
   }, []);
   // To fix this, we should use useEffect to fetch the data only once when the component mounts.
   return (
