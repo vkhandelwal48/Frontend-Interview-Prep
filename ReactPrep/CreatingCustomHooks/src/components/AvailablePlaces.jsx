f84d1e219f52d3886ec6b95b093895b8085a4908
Vidit Khandelwal <59656953+vkhandelwal48@users.noreply.github.com>
2026-07-07 23:16:51 +0530

    Handling HTTP erros

--- ReactPrep/CreatingCustomHooks/src/components/AvailablePlaces.jsx
+++ ReactPrep/CreatingCustomHooks/src/components/AvailablePlaces.jsx
@@ -1,17 +1,29 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
+import ErrorComponent from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState(null);
 
   // useEffect(async () => {}) // we can't use async directly in useEffect, we can create an async function inside useEffect and call it.
   useEffect(() => {
     setIsFetching(true);
+
     async function fetchPlaces() {
-      const response = await fetch('http://localhost:3000/places');
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const response = await fetch('http://localhost:3000/places');
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch places');
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({ message: error.message || 'Could not fetch places, please try again later' });
+      }
       setIsFetching(false);
     }
     fetchPlaces();
@@ -19,6 +31,11 @@ export default function AvailablePlaces({ onSelectPlace }) {
     // This code will create an infinite loop because the state is being updated on every render.
   }, []);
   // To fix this, we should use useEffect to fetch the data only once when the component mounts.
+
+  if (error) {
+    return <ErrorComponent title="An error occurred!" message={error.message} onConfirm={() => setError(null)} />;
+  }
+
   return (
     <Places
       title="Available Places"
